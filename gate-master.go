c61747d670af23b4fbda97f1cef7d0ee1b24fa90
Nicholas Page <npage85@gmail.com>
2017-04-11 12:16:57 -0700

    Sample ping-pong functionality.

--- gate-master.go
+++ gate-master.go
@@ -2,52 +2,34 @@ package main
 
 import (
   "fmt"
-  "log"
+  // "log"
   "os"
 
   "github.com/nlopes/slack"
+
+  // "github.com/davecgh/go-spew/spew"
 )
 
 func main() {
   api := slack.New(os.Getenv("SLACK_TOKEN"))
-  logger := log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)
-  slack.SetLogger(logger)
-  api.SetDebug(true)
 
   rtm := api.NewRTM()
   go rtm.ManageConnection()
 
   for msg := range rtm.IncomingEvents {
-    fmt.Print("Event Received: ")
     switch ev := msg.Data.(type) {
-    case *slack.HelloEvent:
-      // Ignore hello
-
-    case *slack.ConnectedEvent:
-      fmt.Println("Infos:", ev.Info)
-      fmt.Println("Connection counter:", ev.ConnectionCount)
-      // Replace #general with your Channel ID
-      rtm.SendMessage(rtm.NewOutgoingMessage("Hello world", "#general"))
 
     case *slack.MessageEvent:
-      fmt.Printf("Message: %v\n", ev)
-
-    case *slack.PresenceChangeEvent:
-      fmt.Printf("Presence Change: %v\n", ev)
-
-    case *slack.LatencyReport:
-      fmt.Printf("Current latency: %v\n", ev.Value)
-
-    case *slack.RTMError:
-      fmt.Printf("Error: %s\n", ev.Error())
+      // spew.Dump(ev)
+      if(ev.Text == "ping") {
+        rtm.SendMessage(rtm.NewOutgoingMessage("pong", ev.Channel))
+      }
 
     case *slack.InvalidAuthEvent:
       fmt.Printf("Invalid credentials")
       return
 
     default:
-
-      // Ignore other events..
       // fmt.Printf("Unexpected: %v\n", msg.Data)
     }
   }
